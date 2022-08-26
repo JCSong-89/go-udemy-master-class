84b2801a8bafa81565a6936c849d69e7325760c4
JCSong-89 <gandalfzzing@gmail.com>
2022-08-26 17:10:34 +0900

    [FIX] Sqlc 오류 수정

--- db/query/account.sql
+++ db/query/account.sql
@@ -1,9 +1,8 @@
-/*Account 단일 계정 객체 (one) return  */
 -- name: CreateAccount :one
-INSERT INTO  authors (
+INSERT INTO accounts (
     owner,
     balance,
     currency
 ) VALUES (
-    $1, $2, $3
-) RETURNING *; -- $1, $2, $3 은 인자로 받은 값을 사용한다.
+             $1, $2, $3
+         ) RETURNING *;
